6d63ed2c7156cf7dd194666e005c8cc5470406ca
Devendra Kushwah <idevendra08@gmail.com>
2019-01-27 21:33:56 +0530

    update

--- api/models.py
+++ api/models.py
@@ -19,3 +19,20 @@ class Exchange(models.Model):
 
     def __str__(self):
         return self.name
+
+class Coin(models.Model):
+    id=models.IntegerField(max_length=10)
+    cmc_rank=models.IntegerField(max_length=10)
+    symbol = models.CharField(max_length=200, primary_key=True)
+    name = models.CharField(max_length=200)
+    price_usd = models.CharField(max_length=200)
+    total_supply = models.CharField(max_length=200)
+    max_supply = models.CharField(max_length=200)
+    circulating_supply = models.CharField(max_length=200)
+    change_hour = models.CharField(max_length=200)
+    change_day = models.CharField(max_length=200)
+    change_week = models.CharField(max_length=200)
+    last_update = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.id)+' '+str(self.name)
