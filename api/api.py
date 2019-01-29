67942e6b6092b12430aeaaef79357653f7393f1f
Devendra Kushwah <idevendra08@gmail.com>
2019-01-29 22:38:10 +0530

    Update api.py

--- api/api.py
+++ api/api.py
@@ -4,5 +4,6 @@ cmc_api='308324b0-83b2-4eca-853d-b970f66472e6' #coin market cap
 cl_api='d39af46204819a2704ab3babadbd111f' #currency layer
 ccmp_api='77e21907a80700fb0fcee2f5690baa7183392fbdb72569bf52a004ef3b4b40cc' #crypto compare
 
-#sample request
+#api requests
+#http://apilayer.net/api/live?access_key=d39af46204819a2704ab3babadbd111f
 #https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5000&convert=USD&CMC_PRO_API_KEY=308324b0-83b2-4eca-853d-b970f66472e6
